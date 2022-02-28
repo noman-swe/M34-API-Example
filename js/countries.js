d462c57d5e418057d08be3919c9684f8634f4bde
noman-swe <noman.swe130@gmail.com>
2022-02-28 14:41:34 +0600

    34.3-> dynamic by innerHTML

--- js/countries.js
+++ js/countries.js
@@ -10,12 +10,17 @@ const displayCountries = countries => {
         console.log(country);
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = `Country Name: ${country.name.common}`;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = `City: ${country.capital}`;
-        div.appendChild(p);
+        div.innerHTML = `
+        <h3>Country Name: ${country.name.common}</h3>
+        <p>City: ${country.capital}</p>
+        `
+
+        // const h3 = document.createElement('h3');
+        // h3.innerText = `Country Name: ${country.name.common}`;
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = `City: ${country.capital}`;
+        // div.appendChild(p);
         countriesDiv.appendChild(div);
     }
 }
