db7a655493e550dcb039c248131fd29c0edd2aea
noman-swe <noman.swe130@gmail.com>
2022-03-01 00:57:31 +0600

    34-6 Display dynamic search result using bootstrap cards

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,13 +1,32 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
 
     searchField.value = '';
 
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch (url)
     .then(res => res.json())
-    .then(data => console.log(data.meals))
+    .then(data => displaySearchResult(data.meals))
+}
 
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top img-fluid" alt="..." width="300px">
+                <div class="card-body">
+                    <h5 class="card-title">Name: ${meal.strMeal}</h5>
+                    <p class="card-text">Categoty: ${meal.strCategory}</p>
+                    <p class="card-text">Area: ${meal.strArea}</p>
+                    <p class="card-text">Instructions: ${meal.strInstructions.slice(0, 180)}</p>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    });
 }
