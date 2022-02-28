4369af2ee81cba8ab3160ed43637c205ffa110d6
noman-swe <noman.swe130@gmail.com>
2022-02-28 22:44:21 +0600

    34-4 (advanced)[Country-details-flag] Call dynamic API, load dynamic data to display

--- js/countries.js
+++ js/countries.js
@@ -7,21 +7,32 @@ const loadCountries = () => {
 const displayCountries = countries => {
     const countriesDiv = document.getElementById('countriesList');
     for (const country of countries){
-        console.log(country);
+        // console.log(country);
         const div = document.createElement('div');
         div.classList.add('country');
         div.innerHTML = `
         <h3>Country Name: ${country.name.common}</h3>
         <p>City: ${country.capital}</p>
-        `
-
-        // const h3 = document.createElement('h3');
-        // h3.innerText = `Country Name: ${country.name.common}`;
-        // div.appendChild(h3);
-        // const p = document.createElement('p');
-        // p.innerText = `City: ${country.capital}`;
-        // div.appendChild(p);
+         <button onclick="loadCountryByName('${country.name.common}')"> Show details </button>
+        `;
         countriesDiv.appendChild(div);
     }
 }
+
+const loadCountryByName = name =>{
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]));
+} 
+
+const displayCountryDetail = country =>{
+    console.log(country);
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `<h4> ${country.name.common} </h4>
+    <p>Population: ${country.population}</p>
+    <p>region: ${country.region}</p>
+    <img src= "${country.flags.png}" width="200px">
+    `
+} 
 
