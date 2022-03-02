c784356e67533b5e998f7b38040f63c252eeb8ff
noman-swe <noman.swe130@gmail.com>
2022-03-03 01:26:54 +0600

    34-7 Create dynamic url based on click and display data

--- js/mealdb.js
+++ js/mealdb.js
@@ -13,11 +13,11 @@ const searchFood = () => {
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            <div class="card h-100">
+            <div onclick ="loadMealDetail(${meal.idMeal})" class="card h-100">
                 <img src="${meal.strMealThumb}" class="card-img-top img-fluid" alt="..." width="300px">
                 <div class="card-body">
                     <h5 class="card-title">Name: ${meal.strMeal}</h5>
@@ -29,4 +29,31 @@ const displaySearchResult = meals => {
         `;
         searchResult.appendChild(div);
     });
+}
+
+const loadMealDetail = mealId => {
+    // console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetail(data.meals[0]))
+}
+displayMealDetail = meal => {
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('card');
+    div.classList.add('card');
+    div.innerHTML = `
+            <img src="${meal.strMealThumb}" class="card-img-top img-fluid" alt="..." width="">
+            <div class="card-body">
+                <h5 class="card-title">Name: ${meal.strMeal}</h5>
+                <p class="card-text">Categoty: ${meal.strCategory}</p>
+                <p class="card-text">Area: ${meal.strArea}</p>
+                <p class="card-text">Instructions: ${meal.strInstructions.slice(0, 180)}</p>
+                <a class=""></a> </br>
+                <a href="${meal.strYoutube}" target="_blank" type="button" class="btn btn-primary mt-2">Go Somewhere</a>
+                </div>
+    `;
+    mealDetails.appendChild(div);
+
 }
